076a17a8ea8400eaefcae766535cc4581a4af94b
Max Müller <89359847+MMVonnSeek@users.noreply.github.com>
2022-11-18 22:35:57 -0300

    update

--- Spaceshooter/Space-Shooter.py
+++ Spaceshooter/Space-Shooter.py
@@ -69,4 +69,6 @@ def main_menu():
                 pygame.display.update()
 
 #pygame.mixer.muisc.stop()
-ready = pygame.mixer.Sound(path.join(sound_folder,'getready.ogg'))                        
+ready = pygame.mixer.Sound(path.join(sound_folder,'getready.ogg'))
+ready.play()
+                        
