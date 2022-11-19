8b4809beef6ae36e750483d2054a62ba0811ba85
Max Müller <89359847+MMVonnSeek@users.noreply.github.com>
2022-11-19 18:33:28 -0300

    Update Space-Shooter.py

--- Spaceshooter/Space-Shooter.py
+++ Spaceshooter/Space-Shooter.py
@@ -101,3 +101,9 @@ def draw_shield_bar(surf, x, y, pct):
 
     def draw_lives(surf, x, y, lives, img):
         for i in range(lives):
+            img_rect= img.get_rect()
+            img_rect.x = x + 30 * i
+            img_rect.y = y
+            surf.blit(img, img_rect)
+
+
