db39d7e84612087a5959ed8fce2d243b41e64520
Max Müller <89359847+MMVonnSeek@users.noreply.github.com>
2022-11-22 22:02:22 -0300

    Update Space-Shooter.py

--- Spaceshooter/Space-Shooter.py
+++ Spaceshooter/Space-Shooter.py
@@ -294,4 +294,19 @@ class Pow(pygame.sprite.Sprite):
         self.rect.y += self.speedy
         ##  mate o sprite depois que ele passar pela borda superior
         if self.rect.top > HEIGHT:
-            self.kill()                        
+            self.kill()
+
+
+##  define o sprite para balas
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        ##  coloque a bala de acordo com a posição atual do jogador
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):                                                   
